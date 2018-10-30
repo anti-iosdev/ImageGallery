fbbcce1fd196787783a4f951feecdd52fb435893
Anti <anti@antis-imac-pro.local>
2018-10-30 16:39:28 -0700

    will move objects out of the way

--- ImageGallery/ImageGalleryViewController.swift
+++ ImageGallery/ImageGalleryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
+class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate
 {
 
     override func viewDidLoad() {
@@ -28,6 +28,8 @@ class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, 
         didSet {
             imageCollectionView.dataSource = self
             imageCollectionView.delegate = self
+            imageCollectionView.dragDelegate = self
+            imageCollectionView.dropDelegate = self
         }
     }
     var width: CGFloat {
@@ -40,11 +42,7 @@ class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, 
         return emojis.count
     }
     
-    // Setting the Cells
-    private var font: UIFont {
-        let fontSize = width/3
-        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(fontSize))
-    }
+    // Rendering the Cells
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "testCell", for: indexPath)
         if let imageCell = cell as? ImageCollectionViewCell {
@@ -53,10 +51,49 @@ class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, 
         }
         return cell
     }
+    private var font: UIFont {
+        let fontSize = width/3
+        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(fontSize))
+    }
 
     // Size of Cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         //let width = (view.frame.width-20)/2
         return CGSize(width: width, height: width)
     }
+    
+    // Dragging
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return dragItems(at: indexPath)
+    }
+    
+    private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
+        // only works for visible cells
+        if let attributedString = (imageCollectionView.cellForItem(at: indexPath) as? ImageCollectionViewCell)?.label.attributedText {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: attributedString))
+            dragItem.localObject = attributedString
+            return [dragItem]
+        } else {
+            return []
+        }
+    }
+    
+    // MARK: - Dropping Code
+    
+    // Check if it's an attributed string
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return UICollectionViewDropProposal(operation: isSelf ? .move : .copy, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        
+    }
+    
+    
 }
