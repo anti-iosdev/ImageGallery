5f6aeed306ede1c9cda928c935e66ecbbce8dc24
Anti <anti@antis-imac-pro.local>
2018-10-30 14:07:26 -0700

    basic UI implemented

---
+++ ImageGallery/ImageGalleryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageGalleryViewController.swift
+//  ImageGallery
+//
+//  Created by Anti on 10/30/18.
+//  Copyright © 2018 Anti. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
